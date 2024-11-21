6e9c64ad3fbb7b65f92d60295cd9046855fb34a2
Rafael Patrick de Souza <45822152+faelpatrick@users.noreply.github.com>
2024-11-21 10:59:56 +0000

    Update parrot.py

--- Python IA/speech/parrot.py
+++ Python IA/speech/parrot.py
@@ -9,21 +9,28 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
-def speak(engine, text):
+def speak(text):
+    global engine
     engine.say(text)
     engine.runAndWait()
 
 
 # Capturar áudio do microfone
-with sr.Microphone(device_index=2) as source:
-    print("Diga algo...")
-    try:
-        audio = recognizer.listen(source)
-        # Reconhecer usando o Google Speech Recognition
-        text = recognizer.recognize_google(audio, language="pt-PT")
-        print("Você disse:", text)
-        speak(engine, text)
-    except sr.UnknownValueError:
-        print("Não entendi o que você disse.")
-    except sr.RequestError as e:
-        print("Erro ao conectar ao serviço de reconhecimento:", e)
+def listen():
+    with sr.Microphone(device_index=2) as source:
+        print("Diga alguma coisa...")
+        speak("Diga alguma coisa...")
+        try:
+            audio = recognizer.listen(source)
+            # Reconhecer usando o Google Speech Recognition
+            text = recognizer.recognize_google(audio, language="pt-PT")
+            print("Você disse:", text)
+            speak(text)
+        except sr.UnknownValueError:
+            print("Não entendi o que você disse.")
+            speak("Não entendi o que vocé disse.")
+            listen()
+        except sr.RequestError as e:
+            print("Erro ao conectar ao serviço de reconhecimento:", e)
+
+listen()
